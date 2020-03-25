9c1f51c61830cc9b9eda637995c6bbf287a85059
constantinos <constantinides_c@hotmail.com>
2020-03-25 16:09:55 +0200

    Created getMyLocation functionality

--- active-citizen-app/src/app/shared/component/my-location-map/my-location-map.component.ts
+++ active-citizen-app/src/app/shared/component/my-location-map/my-location-map.component.ts
@@ -34,7 +34,11 @@ export class MyLocationMapComponent implements AfterViewInit {
     const defaultPosition = new google.maps.LatLng(
       35.185471,
       33.389757);
+    this.getMyLocation();
+    this.mapInitializer(defaultPosition);
+  }
 
+  getMyLocation() {
     navigator.geolocation.getCurrentPosition(position => {
       console.log('My Position accuracy: ' + position.coords.accuracy);
       const myPosition = new google.maps.LatLng(
@@ -43,11 +47,11 @@ export class MyLocationMapComponent implements AfterViewInit {
       this.mapInitializer(myPosition);
     });
 
-    this.mapInitializer(defaultPosition);
   }
 
   mapInitializer(myPosition: google.maps.LatLng): void {
     this.map = this.setupMap(myPosition);
+    this.createMyLocationControl(this.map);
     this.setupMarker(myPosition, this.map);
   }
 
@@ -125,6 +129,41 @@ export class MyLocationMapComponent implements AfterViewInit {
         this.onAddressUpdated('Unable to get address');
       }
     });
+  }
+
+
+
+  createMyLocationControl(map: google.maps.Map): void {
+
+    const getMyLocationDiv = document.createElement('div');
+    getMyLocationDiv.style.backgroundColor = '#fff';
+    getMyLocationDiv.style.border = '2px solid #fff';
+    getMyLocationDiv.style.borderRadius = '3px';
+    getMyLocationDiv.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    getMyLocationDiv.style.cursor = 'pointer';
+    getMyLocationDiv.style.textAlign = 'center';
+    getMyLocationDiv.style.marginRight = '10px';
+    getMyLocationDiv.style.height = '40px';
+    getMyLocationDiv.style.width = '40px';
+    getMyLocationDiv.style.display = 'flex';
+    getMyLocationDiv.style.alignItems = 'center';
+    getMyLocationDiv.style.justifyContent = 'center';
+    getMyLocationDiv.title = 'Get my location';
+
+    const getMyLocationIcon = document.createElement('div');
+    getMyLocationIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg"'
+      + ' width="24" height="24" viewBox="0 0 24 24"><path d="M12 8c-2.21 0-4 ' +
+      '1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.' +
+      '94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 ' +
+      '7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>';
+
+    getMyLocationDiv.appendChild(getMyLocationIcon);
+
+    getMyLocationDiv.addEventListener('click', () => {
+      this.getMyLocation();
+    });
+
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(getMyLocationDiv);
   }
 
 }
